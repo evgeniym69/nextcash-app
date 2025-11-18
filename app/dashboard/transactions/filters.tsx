58b278216d6e0af8dc186ce5c9395a114c5a7929
Yevhenii <evgeniym69@gmail.com>
2025-11-19 01:33:44 +0200

    [+] added transactions table; added dropdowns; added years range

---
+++ app/dashboard/transactions/filters.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { format } from "date-fns";
+import Link from "next/link";
+import { useState } from "react";
+
+const Filters = ({
+  year,
+  month,
+  yearsRange,
+}: {
+  year: number;
+  month: number;
+  yearsRange: number[];
+}) => {
+  const [selectedMonth, setSelectedMonth] = useState(month);
+  const [selectedYear, setSelectedYear] = useState(year);
+
+  return (
+    <div className="flex gap-2">
+      <Select
+        value={selectedMonth.toString()}
+        onValueChange={(newValue) => setSelectedMonth(+newValue)}
+      >
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {Array.from({ length: 12 }).map((_, i) => (
+            <SelectItem value={`${i + 1}`} key={i}>
+              {format(new Date(0, i, 1), "MMM")}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Select
+        value={selectedYear.toString()}
+        onValueChange={(newValue) => setSelectedYear(+newValue)}
+      >
+        <SelectTrigger>
+          <SelectValue />
+        </SelectTrigger>
+        <SelectContent>
+          {yearsRange.map((year) => (
+            <SelectItem value={year.toString()} key={year}>
+              {year}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <Button asChild>
+        <Link
+          href={`/dashboard/transactions?year=${selectedYear}&month=${selectedMonth}`}
+        >Go</Link>
+      </Button>
+    </div>
+  );
+};
+
+export default Filters;
